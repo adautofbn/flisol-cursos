f15507c5a61c571859e4aa3b1f5cdebb6a63be3a
Adauto Barros <adauto.neto@ccc.ufcg.edu.br>
2019-04-27 11:38:48 -0300

    virtual assistant via terminal

--- iris.py
+++ iris.py
@@ -1,1 +1,39 @@
-app_id = "HJ2Q67-7A35YVXQ98"
+import wolframalpha as wa
+import datetime as dt
+import getpass as gp
+import wikipedia as wp
+
+user = gp.getuser()
+
+def getMessage():
+    time = dt.datetime.now().hour
+    message = "Good Morning, " if (time < 12) else ("Good Afternoon, " if (time < 18) else "Good Evening, ")
+    message += user.capitalize()
+    return message
+
+def wolframQuestion(input):
+    answer = "\n"
+    app_id = "HJ2Q67-7A35YVXQ98"
+    client = wa.Client(app_id)
+    result = client.query(input)
+    answer += next(result.results).text
+    answer += "\nsource: WolframAlpha\n"
+    return answer
+
+def wikipediaQuestion(input):
+    answer = "\n"
+    answer += wp.summary(input, sentences=2)
+    answer += "\nsource: Wikipedia\n"
+    return answer
+
+print getMessage()
+
+want = "yes"
+
+while want == "yes" or want == "y":
+    input = raw_input("Question: ")
+    try:
+        print wolframQuestion(input)
+    except:
+        print wikipediaQuestion(input)
+    want = raw_input("Wanna ask a question? (yes/no): ")
